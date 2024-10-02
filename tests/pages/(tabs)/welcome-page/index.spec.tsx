70b982d7a9fdbf0ed7836924f26a3da5ce18e155
bartek3060 <bartek30603060@onet.pl>
2024-10-02 23:57:25 +0200

    fix ut 2

--- tests/pages/(tabs)/welcome-page/index.spec.tsx
+++ tests/pages/(tabs)/welcome-page/index.spec.tsx
@@ -2,14 +2,23 @@ import { render, screen } from "@testing-library/react-native";
 import WelcomePage from "../../../../app/(pages)/welcome-page";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+jest.mock("../../../../app/api/hooks/test/useGetTest", () => ({
+  useGetTest: () => ({
+    data: {
+      userId: 1,
+      id: 1,
+      title: "delectus aut autem",
+      completed: false,
+    },
+    isLoading: false,
+    isError: false,
+  }),
+}));
+
 describe("WelcomePage", () => {
   test("should display title", async () => {
     // Given
-    await render(
-      <QueryClientProvider client={new QueryClient()}>
-        <WelcomePage />
-      </QueryClientProvider>
-    );
+    await render(<WelcomePage />);
 
     // When, Then
     screen.getByText("Hello login to use app");
